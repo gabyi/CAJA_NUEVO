25b8868dbbbb267adbb21d359152342388e1004f
gabyi <gabrielisabella@hotmail.com>
2015-06-05 13:29:23 -0300

    saque los href

--- footer.php
+++ footer.php
@@ -8,9 +8,9 @@
         		<div class="col-sm-6 col-md-6">
                 	<h4 class="widget-title">Utilidades</h4>
 						<ul class="menu">
-                			<li class="menu-item"><a href="http://pearanalytics.com/locate-us/">Tasa mix</a></li>
-   							<li class="menu-item"><a href="http://pearanalytics.com/locate-us/">C&aacute;lculo de intereses</a></li>
-							<li class="menu-item"><a href="http://webmail.cforense.org/webmail2">Ingreso a su cuenta de mail</a></li>
+                			<li class="menu-item"><a href="">Tasa mix</a></li>
+   							<li class="menu-item"><a href="">C&aacute;lculo de intereses</a></li>
+							<li class="menu-item"><a href="">Ingreso a su cuenta de mail</a></li>
 						</ul>
 
             	</div><!-- /.widget -->
@@ -20,9 +20,9 @@
                 <h4 class="widget-title">Otros links</h4>
 
                 	<ul class="menu">
-                			<li class="menu-item"><a href="http://pearanalytics.com/locate-us/">Novedades</a></li>
-   							<li class="menu-item"><a href="http://pearanalytics.com/locate-us/">Tasa mix</a></li>
-							<li class="menu-item"><a href="/meet-the-team">C&aacute;lculo de intererses</a></li>
+                			<li class="menu-item"><a href="">Novedades</a></li>
+   							<li class="menu-item"><a href="">Tasa mix</a></li>
+							<li class="menu-item"><a href="">C&aacute;lculo de intererses</a></li>
 					</ul>
 
 				</div><!-- /.widget -->		
