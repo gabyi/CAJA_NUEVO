47bb7470fd65a6541615b95d5f2b9d5196d1ac04
gabyi <gabrielisabella@hotmail.com>
2016-08-03 18:12:09 -0300

    ESTILOS Y CAMBIOS EN INDEX, CON LAS FOTOS

--- index.php
+++ index.php
@@ -53,15 +53,23 @@ session_start();
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div  class="item active">
-      <div id="texto-foto" class="col-sm-6 col-md-6">
-        <h3>BENEFICIOS CON COMERCIOS LOCALES</h3>
-        <p>Para más informacion sobre estos beneficios <a href="archivos/descuentos.jpg">ver imagen</a>.</p>
-      </div>
-      <div id="foto-foto" class="col-sm-6 col-md-6">
-        <img class="img-thumbnail" src="archivos/descuentos.jpg" alt="Chania">
+      <div id="item" class="">
+        <a href="imagenes/slider/FLYERCOMERCIOS.jpg"><img class="img-thumbnail" style="width:90%; height:90%" src="imagenes/slider/FLYERCOMERCIOS.jpg" alt="Chania"></a>
       </div>
     </div>
 
+    <div  class="item">
+      <div id="item" class="">
+        <a href="imagenes/slider/FLYER BONO MEDIACION Y CONSULTA.jpg"><img class="img-thumbnail" style="width:90%; height:90%" src="imagenes/slider/FLYER BONO MEDIACION Y CONSULTA.jpg" alt="Chania"></a>
+      </div>
+    </div>
+
+    <div  class="item">
+      <div id="item" class="">
+        <a href="imagenes/slider/BENEFICIOS GENERAL.jpg"><img class="img-thumbnail" style="width:90%; height:90%" src="imagenes/slider/BENEFICIOS GENERAL.jpg" alt="Chania"></a>
+      </div>
+    </div>
+<!--
     <div class="item">
       <div id="texto-foto" class="col-sm-6 col-md-6">
         <h3>Taller de Mediaci&oacute;n</h3>
@@ -95,7 +103,7 @@ session_start();
       </div>
     </div>
     </div>
-
+-->
 
   <!-- Left and right controls -->
   <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
@@ -116,12 +124,12 @@ session_start();
           <div class="col-sm-4 col-md4">
             <div id="panel-noticia" class="panel panel-default">
                 <div id="panel-titulo-noticia" class="panel-heading" style="background: url(imagenes/logos/fondo_azul.png);">
-                  <h3>Boletín Infotmativo N° 42</h3>
+                  <h3>Nuevos Haberes Previsionales</h3>
                 </div>
                 <div id="panel-cuerpo-noticia" class="panel-body">
-                   <p>Tenemos el agrado de dirigirnos a Ud. a fin de remitirle adjunto el Boletín Informativo N° 42, 
-                    en relación al taller de Comediación Familiar a Distancia que se dictó el día 11 de Junio en la 
-                    sede del Colegio de Abogados y Procuradores de la provincia de La Pampa.</p>
+                   <p>El 1° de septiembre próximo entrarán en vigencia los nuevos haberes previsionales establecidos
+                    por el Directorio. El segundo aumento en el año para las jubilaciones y pensiones se implementará
+                    mediante un subsidio no remunerativo a fin de morigerar su impacto en los aportes anuales.</p>
                    <button id="boton-noticia" type='button' class='btn btn-info  btn-lg' name='' style="background: url(imagenes/logos/fondo_azul.png);" onClick="irA('BOLETIN_INFORMATIVO_42.pdf')">Ver Mas</button>
                 </div>
             </div>
@@ -131,13 +139,13 @@ session_start();
         <div class="col-sm-4 col-md4">
             <div id="panel-noticia" class="panel panel-default">
                 <div id="panel-titulo-noticia" class="panel-heading" style="background: url(imagenes/logos/fondo_azul.png);">
-                  <h3>Boletín Infotmativo N° 41</h3>
+                  <h3>Valor Técnico Actuarial</h3>
                 </div>
                 <div id="panel-cuerpo-noticia" class="panel-body">
-                   <p>Tenemos el agrado de dirigirnos a Ud. a fin de remitirle adjunto el Boletín Informativo N° 41, 
-                    en relación con la charla de la Ley de Mediación Prejudicial Obligatoria realizada el día 12 de 
-                    mayo próximo pasado y organizada por la Comisión de Jóvenes de Caja Forense junto con el Centro 
-                    de Mediación de Santa Rosa y el Colegio de Abogados.</p>
+                   <p>A partir del corriente mes se implementó un nuevo método de cálculo del Valor Técnico Actuarial,
+                    con el objetivo de castigar la falta de solidaridad y desalentar la morosidad y fue desarrollado
+                    en base al informe técnico titulado Factor de Actualización Actuarial elaborado por el Estudio de 
+                    Actuarios Fastman y Asociados.</p>
                    <button id="boton-noticia" type='button' class='btn btn-info  btn-lg' name='' style="background: url(imagenes/logos/fondo_azul.png);" onClick="irA('BOLETIN_INFORMATIVO_41.pdf')">Ver Mas</button>
                 </div>
             </div>
@@ -146,12 +154,13 @@ session_start();
           <div class="col-sm-4 col-md4">
             <div id="panel-noticia" class="panel panel-default">
                 <div id="panel-titulo-noticia" class="panel-heading" style="background: url(imagenes/logos/fondo_azul.png);">
-                  <h3>Boletín Infotmativo N° 40</h3>
+                  <h3>Bono Consulta</h3>
                 </div>
                 <div id="panel-cuerpo-noticia" class="panel-body">
-                   <p>Tenemos el agrado de dirigirnos a Ud. a fin de remitirle adjunto el Boletín Informativo N° 40, 
-                    con información relacionada con el criterio adoptado por la DGR para la liquidación de la Tasa 
-                    de Justicia en los juicios promovidos con Beneficio de Litigar sin Gastos.</p>
+                   <p>Respondiendo a la solicitud de muchos afiliados y con la finalidad de facilitar su utilización,
+                    se modificó el valor unitario del bono, habiéndoselo fijado en UN UMED, unidad de medida que
+                    permitirá mantener su poder adquisitivo. Los nuevos talonarios de Bonos Consulta estarán a
+                    disposición de los interesados a partir del 16 de agosto próximo.</p>
                    <button id="boton-noticia" type='button' class='btn btn-info  btn-lg' name='' style="background: url(imagenes/logos/fondo_azul.png);" onClick="irA('BOLETIN_INFORMATIVO_40.pdf')">Ver Mas</button>
                 </div>
             </div>
