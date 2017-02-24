e5609e0d0c271c1725a2854cb9d86cb7eb5df027
gabyi <gabrielisabella@hotmail.com>
2017-02-24 10:34:42 -0300

    hace el excel con la tasa mix desde un boton
    hay que adaptarlo y mejorar las vistas de las tasa
    podria probar con hacer la tabla diferente y hacer un acumulado y solo
    imprimir la tabla como en los costos de juicio

---
+++ ejemplo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<a href="exportar.php">    
+    <button class="btn btn-primary" >
+                Exportar
+    </button>
+</a>
+</body>
+</html>
